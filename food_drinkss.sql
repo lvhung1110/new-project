ca5990af52a15d16c39c8cfdcd11bb8a624ee2f1
lvhung1110 <129417007+lvhung1110@users.noreply.github.com>
2023-04-11 07:48:48 +0700

    Delete food_drinkss.sql

--- food_drinkss.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE nha_hang.food_drinks(
-	food_drinks_id int not null,
-	food_drinks_name varchar(50),
-	PRICE FLOAT,
-	CONSTRAINT pk_food_drinks PRIMARY KEY(food_drinks_id)
-	
-);
-
-SELECT * FROM nha_hang.food_drinks
