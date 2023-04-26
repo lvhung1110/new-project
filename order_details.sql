512d6c18f5729945bd6647eea42d45ffada58023
lvhung1110 <129417007+lvhung1110@users.noreply.github.com>
2023-04-27 06:20:24 +0700

    Delete order_details.sql

--- order_details.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE restaurant.order_details(
-	order_id int not null,
-	food_drink_id int not null,
-	quantity int not null,
-	price real,
-	primary key(order_id,food_drink_id)
-);
-SELECT * FROM restaurant.order_details
-
